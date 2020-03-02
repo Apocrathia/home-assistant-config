1b1d2ae07734baa9fe7a883d1f1b8e350e90488f
Apocrathia <ian@apocrathia.com>
2020-03-02 09:17:02 -0700

    More Travis-CI fixes

--- .travis/custom_components.sh
+++ .travis/custom_components.sh
@@ -3,5 +3,13 @@
 # This script is to setup various custom components to allow 
 # configuration checks to pass.
 
+# Create custom_components directory
+mkdir./custom_components
+
 # Home Assistant Remote
-git clone https://github.com/lukas-hetzenecker/home-assistant-remote custom_components/remote_homeassistant
+git clone https://github.com/lukas-hetzenecker/home-assistant-remote /tmp/remote_homeassistant
+mv /tmp/remote_homeassistant/custom_components/remote_homeassistant ./custom_components/remote_homeassistant 
+
+# Auto Backup
+git clone https://github.com/jcwillox/hass-auto-backup /tmp/auto_backup
+mv /tmp/auto_backup/custom_components/auto_backup ./custom_components/auto_backup
