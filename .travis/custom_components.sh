4d0f3a2da71f71fd5db029805debe39a60bf7f42
Apocrathia <ian@apocrathia.com>
2020-03-02 09:02:42 -0700

    Attempting to fix Travis-CI

---
+++ .travis/custom_components.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script is to setup various custom components to allow 
+# configuration checks to pass.
+
+# Home Assistant Remote
+git clone https://github.com/lukas-hetzenecker/home-assistant-remote custom_components/remote_homeassistant
