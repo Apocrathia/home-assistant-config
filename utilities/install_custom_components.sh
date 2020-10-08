4dc8b372a1e074dea896abbc4dde39ca2de06aef
Apocrathia <ian@apocrathia.com>
2020-10-08 14:04:18 -0600

    I should deploy a local runner to test this.

--- utilities/install_custom_components.sh
+++ utilities/install_custom_components.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # To be run from the root of the config folder
+echo "Working out of $PWD"
 
 # Make custom_components folder
-mkdir custom_components
+mkdir -v custom_components
 
 # Enter custom_components folder
+echo "Entering custom_components directory."
 cd custom_components
 
 # Install custom components
@@ -13,14 +15,14 @@ cd custom_components
 # alexa_media
 git clone https://github.com/custom-components/alexa_media_player
 mv alexa_media_player/custom_components/alexa_media alexa_media
-rmdir alexa_media_player
+rm -rfv alexa_media_player
 
 # auto_backup
 git clone https://github.com/jcwillox/hass-auto-backup
 mv hass-auto-backup/custom_components/auto_backup auto_backup
-rmdir hass-auto-backup
+rm -rfv hass-auto-backup
 
 # remote_homeassistant
 git clone https://github.com/lukas-hetzenecker/home-assistant-remote
 mv home-assistant-remote/custom_components/remote_homeassistant remote_homeassistant
-rmdir home-assistant-remote
+rm -rfv home-assistant-remote
